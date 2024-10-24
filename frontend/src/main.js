53a8579bdd0300b6f55aad4ef23f3ba65ab087f1
kirthivasshni <152830118+kirthivasshni@users.noreply.github.com>
2024-10-24 13:05:23 +0800

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,19 +2,22 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
-import List from './components/Recipes/List.vue';
-import Details from './components/Recipes/Details.vue';
+import List from './components/Recipes/RecipeList.vue';
+import Details from './components/Recipes/RecipeDetails.vue';
 
 const routes = [
-  { path: '/', component: List },
+  { path: '/', component: List }, 
   { path: '/recipe/:id', name: 'RecipeDetails', component: Details }
 ];
 
+
 const router = createRouter({
   history: createWebHistory(),
-  routes,
+  routes, 
 });
 
 const app = createApp(App);
-app.use(router);
-app.mount('#app');
+
+app.config.globalProperties.items = []; 
+
+app.use(router).mount('#app');
