a1a83f19e60ecd4ef0dd0a441b0dd377f0853f1f
kirthivasshni <152830118+kirthivasshni@users.noreply.github.com>
2024-10-24 13:05:52 +0800

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,15 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-
-createApp(App).mount('#app');
+<template>
+    <div id="app">
+      <router-view></router-view> <!-- This displays routed components -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'App'
+  }
+  </script>
+  
+  <style>
+  /* Add global styles here if necessary */
+  </style>
