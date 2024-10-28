46fed4037c8900ee656dccfd8e7bca1189063384
zzelos <jiaying.yap.2023@scis.smu.edu.sg>
2024-10-28 11:48:58 +0800

    ?????????

--- frontend/src/pages/CommunityPage.vue
+++ frontend/src/pages/CommunityPage.vue
@@ -1,1 +1,17 @@
-<template>Community</template>
+<template>
+  <div class="community-page">
+   
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+
+export default defineComponent({
+  name: 'CommunityPage',
+  components: {
+   
+  }
+})
+</script>
