6f09263a039703aee970dc9ec05cef2ba9c9662d
zzelos <jiaying.yap.2023@scis.smu.edu.sg>
2024-11-04 11:27:38 +0800

    sortbylike

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -41,7 +41,7 @@ const routes = [
 
   {
     path: '/recipe/:id',
-    name: 'RecipeDetail',
+    name: 'RecipeDetailPage',
     component: RecipeDetailPage,
     props: true, // allows `id` to be passed as a prop
   },
